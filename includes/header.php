a8e160eb906492b8655b55faec494423d6e1fec6
EtecAB <you@example.com>
2020-02-27 16:18:41 -0300

    Arquivos TCC

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/dev.css">
+	<link rel="stylesheet" type="text/css" href="css/tcc.css">
+
+	<title>TCC Viewer</title>
+</head>
+<body>
+
+	<!--Side nav-->
+	<ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="img/perfil.png" id="fundoPerfil" class="responsive-img">
+      </div>
+      <a id="imgPerfil" href="perfil.html"><img class="circle responsive-img" src="img/perfil.png"></a>
+    </div></li>
+		<li><p class="center text black-text">Daniel Lino de Almeida</p></li>
+		<li><a href="#!">Second Link</a></li>
+		<li><div class="divider"></div></li>
+		<li><a class="subheader">Subheader</a></li>
+		<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+	</ul>
+	<nav>
+		<div class="nav-wrapper">
+			<img id="imgMenu" src="img/menu.png"  data-target="slide-out" class="sidenav-trigger left">
+			<a href="#" class="brand-logo"><b>Desenvolvimento</b></a>
+		</div>
+	</nav>
+	    <!--Fim da Side Nav-->
---
+++ tcc.php
@@ -0,0 +1,18 @@
+<?php
+	include_once "includes/header.php";
+	include_once "includes/rodape.php";
+?>
+  <div class="row">
+    <div class="col s12 m6">
+      <div class="card small">
+        <div class="card-image">
+          <img src="img/perfil.png" class="img-responsive">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
